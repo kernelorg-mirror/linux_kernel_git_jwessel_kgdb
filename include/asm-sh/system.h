570a304d379af398f0780d3e6d19065988d1a532
Jason Wessel <jason.wessel@windriver.com>
2007-06-26 09:36:27 -0500

    sh-lite.patch Subject: [PATCH] This adds basic support for KGDB on SuperH
    This patch also adds some architecture specific notes to the DocBook
    file and converting the 7751 to use this.  I have tested all
    combinations of 8250 and SCI(F) ports being used as KGDB and console
    that I could (one of each usable to me).
    
    The sh-sci.c file needs to be ported forward as it is known to be broken for KGDB...
    
    
    Signed-off-by: Milind Dumbare <milind@linsyssoft.com>
    Signed-off-by: Tom Rini <trini@kernel.crashing.org>

--- include/asm-sh/system.h
+++ include/asm-sh/system.h
@@ -264,6 +264,45 @@ extern unsigned int instruction_size(unsigned int insn);
 #define instruction_size(insn)	(2)
 #endif
 
+static inline unsigned long __cmpxchg_u32(volatile int * m, unsigned long old,
+	unsigned long new)
+{
+	__u32 retval;
+	unsigned long flags;
+
+	local_irq_save(flags);
+	retval = *m;
+	if (retval == old)
+		*m = new;
+	local_irq_restore(flags);       /* implies memory barrier  */
+	return retval;
+}
+
+/* This function doesn't exist, so you'll get a linker error
+ * if something tries to do an invalid cmpxchg(). */
+extern void __cmpxchg_called_with_bad_pointer(void);
+
+#define __HAVE_ARCH_CMPXCHG	1
+
+static inline unsigned long __cmpxchg(volatile void * ptr, unsigned long old,
+		unsigned long new, int size)
+{
+	switch (size) {
+	case 4:
+		return __cmpxchg_u32(ptr, old, new);
+	}
+	__cmpxchg_called_with_bad_pointer();
+	return old;
+}
+
+#define cmpxchg(ptr,o,n)						 \
+  ({									 \
+     __typeof__(*(ptr)) _o_ = (o);					 \
+     __typeof__(*(ptr)) _n_ = (n);					 \
+     (__typeof__(*(ptr))) __cmpxchg((ptr), (unsigned long)_o_,		 \
+				    (unsigned long)_n_, sizeof(*(ptr))); \
+  })
+
 /* XXX
  * disable hlt during certain critical i/o operations
  */
